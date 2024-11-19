7c266f133a0bbe3073505667152cae4b41bc1970
Катя <katja48tab42794@gmail.com>
2024-11-19 13:56:45 +0300

    init 19/11

---
+++ main.py
@@ -0,0 +1,47 @@
+# This is a sample Python script.
+# Это пример скрипта на Python.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Нажмите Shift+F10, чтобы запустить его или заменить своим кодом.
+
+# Press Double Shift to search everywhere for classes, files, tool windows, actions,
+# and settings.
+# Нажмите двойную комбинацию клавиш для поиска классов, файлов, окон инструментов, действий
+# и настроек по всему миру.
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+#def print_hi(имя):
+    # Используйте точку останова в приведенной ниже строке кода для отладки вашего скрипта.
+#    print(f'Hi, {name}') # Нажмите Ctrl+F8, чтобы переключить точку останова.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+print('#если __name__ == "__main__"')
+print("#print_hi('PyCharm')")
+
+# Нажмите зеленую кнопку в желобе, чтобы запустить скрипт.
+#если __name__ == '__main__':
+#    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Смотрите справку по PyCharm по адресу https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+
+
+
+
